8f276e6bec5cd78d6b6d2591dbd6b19b7d2f0f94
Krishanu Das <dkrishanu94@gmail.com>
2025-08-30 10:56:34 +0530

    changes base in vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// This ensures localhost works normally, and GitHub Pages has correct base
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: command === "build" ? "/LoanSimplify/" : "/",
+    base: '/Loansimplify/', 
   };
 });
